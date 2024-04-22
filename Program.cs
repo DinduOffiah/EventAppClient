764ba6e2b3defd5520a89110f1400fc42669d70d
Chidindu <chidinduoffiah@gmail.com>
2024-04-22 14:57:21 +0100

    Added Detailed Error Messages to Console

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Configuration.Bind("DetailedErrors", options => builder.HostEnvironment.IsDevelopment());
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7259/") });
 
 await builder.Build().RunAsync();

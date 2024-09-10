871a346f124c2000e8d7007759079de31f772442
Chidindu <chidinduoffiah@gmail.com>
2024-09-10 16:41:57 +0100

    Add email to registration

--- Pages/RegisterModel.cs
+++ Pages/RegisterModel.cs
@@ -8,6 +8,10 @@ namespace EventAppClient.Pages
         [StringLength(50, ErrorMessage = "Username must be between 3 and 50 characters.", MinimumLength = 3)]
         public string Username { get; set; }
 
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; }
+
         [Required(ErrorMessage = "Password is required.")]
         [StringLength(100, ErrorMessage = "Password must be between up to 6 characters.", MinimumLength = 6)]
         public string Password { get; set; }

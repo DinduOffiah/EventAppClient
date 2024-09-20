33abad958ab38f4dfb09b3cab04e53766b270b7e
Chidindu <chidinduoffiah@gmail.com>
2024-09-20 13:08:10 +0100

    Make login use either username or email

--- Pages/LoginModel.cs
+++ Pages/LoginModel.cs
@@ -6,7 +6,7 @@ namespace EventAppClient.Pages
     {
         [Required(ErrorMessage = "Username is required.")]
         [StringLength(50, ErrorMessage = "Username must be between 3 and 50 characters.", MinimumLength = 3)]
-        public string Username { get; set; }
+        public string UsernameOrEmail { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
         [StringLength(100, ErrorMessage = "Password must be up to 6  characters.", MinimumLength = 6)]
